30a871b5e7d14099f1641c76161618134e4b60c2
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2017-09-11 13:12:44 -0500

    Fixing Github ApplyFunctionTaggedCollection

---
+++ defs/src/unalcol/math/function/ApplyFunctionTaggedCollection.java
@@ -0,0 +1,33 @@
+package unalcol.math.function;
+
+import java.util.Iterator;
+
+import unalcol.Tagged;
+import unalcol.types.collection.Collection;
+
+public class ApplyFunctionTaggedCollection<S,T>  implements Collection<T>{
+	protected Function<S,T> function;
+	protected Collection<Tagged<S>> collection=null;
+    
+	protected class ApplyFunctionTaggedCollectionIterator implements Iterator<T>{
+		protected Iterator<Tagged<S>> iterator;
+		public ApplyFunctionTaggedCollectionIterator(){ this.iterator = collection.iterator(); }
+		
+		@Override
+		public boolean hasNext(){ return iterator.hasNext(); }
+		
+		@Override
+		public T next(){ return function.apply(iterator.next()); }
+	}
+    
+	public ApplyFunctionTaggedCollection(Function<S,T> function, Collection<Tagged<S>> collection) {
+		this.function = function;
+		this.collection = collection;
+	} 
+    
+	@Override
+	public Iterator<T> iterator(){ return new ApplyFunctionTaggedCollectionIterator(); }
+
+	@Override
+	public boolean isEmpty(){ return collection.isEmpty(); }
+}
