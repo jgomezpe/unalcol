2cc8338184a3b43228420b34dbf23ed4b3ef3053
Jonatan Gomez <jgomezpe@unal.edu.co>
2017-10-11 22:59:58 -0500

    Bug (style change uses token length not endOffset) SyntaxEditPanel fixed

--- gui/src/unalcol/gui/editor/SyntaxEditPanel.java
+++ gui/src/unalcol/gui/editor/SyntaxEditPanel.java
@@ -102,7 +102,7 @@ public class SyntaxEditPanel extends JTextPane implements SyntaxEditComponent{
 			@Override
 				public void run() {
 					for( Token t:changes ){
-						doc.setCharacterAttributes(t.pos().offset(),t.endOffset(),doc.getStyle(token_style.get(t.type())),true);
+						doc.setCharacterAttributes(t.pos().offset(),t.length(),doc.getStyle(token_style.get(t.type())),true);
 	    			}
 				}
 	        };
